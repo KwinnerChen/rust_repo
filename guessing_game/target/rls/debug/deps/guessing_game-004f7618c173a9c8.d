4afdce6d2ee3c607395dd6bb53efbfc403102d9d
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-04 19:51:38 +0800

    update:一个简单的猜数

--- guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.d
+++ guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.d
@@ -1,5 +1,5 @@
-/Users/hafu/Desktop/rust_project/guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.rmeta: src/main.rs
+/Users/hafu/Desktop/rust_repo/guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.rmeta: src/main.rs
 
-/Users/hafu/Desktop/rust_project/guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.d: src/main.rs
+/Users/hafu/Desktop/rust_repo/guessing_game/target/rls/debug/deps/guessing_game-004f7618c173a9c8.d: src/main.rs
 
 src/main.rs:
