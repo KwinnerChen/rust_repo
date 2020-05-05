77e63d154962a501b34e0d4cff89f99b554dbb5b
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-05 23:18:08 +0800

    update: hello rust

---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/Users/hafu/Desktop/rust_repo/hello_rust/target/debug/hello_rust: /Users/hafu/Desktop/rust_repo/hello_rust/src/main.rs
---
+++ hello_rust/target/rls/debug/deps/hello_rust-e2b1cea46931abb3.d
@@ -0,0 +1,5 @@
+/Users/hafu/Desktop/rust_repo/hello_rust/target/rls/debug/deps/hello_rust-e2b1cea46931abb3.rmeta: src/main.rs
+
+/Users/hafu/Desktop/rust_repo/hello_rust/target/rls/debug/deps/hello_rust-e2b1cea46931abb3.d: src/main.rs
+
+src/main.rs:
