d80073073936d04c62918caa22e2d801ea3e0be5
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-06 18:09:44 +0800

    update

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main()
 {
     println!("说出你想的一个数字：");
 
     let mut guess = String::new();
+    let secret_num = rand::thread_rng().gen_range(1, 101);
 
     io::stdin()
         .read_line(&mut guess)
         .expect("读取输入出错了！");
 
     println!("你想的是：{}", guess);
+    println!("随机数：{}", secret_num);
 }
