19204fc0ab172d1fb92c9994e68dcf4687140cf3
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-06 18:35:03 +0800

    update:添加随机数与给定数的比较

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main()
 {
@@ -8,10 +9,18 @@ fn main()
     let mut guess = String::new();
     let secret_num = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
+    io::stdin()                                                         // 标准输入读取的都是字符串
         .read_line(&mut guess)
         .expect("读取输入出错了！");
 
+    let guess: i32 = guess.trim().parse().expect("需要输入一个数字！");    // 覆盖变量，进行类型转换
+
     println!("你想的是：{}", guess);
     println!("随机数：{}", secret_num);
+
+    match guess.cmp(&secret_num) {
+        Ordering::Equal => println!("猜中了！"),
+        Ordering::Less => println!("猜小了！"),
+        Ordering::Greater => println!("猜大了！"),
+    }
 }
