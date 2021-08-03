4578964c0df704b153222bc4fb7ce8dab3251ea6
KwinnerChen <chenkai.c.k@hotmail.com>
2021-08-03 14:12:00 +0800

    update: rocket server

--- hello_rocket/src/main.rs
+++ hello_rocket/src/main.rs
@@ -5,10 +5,16 @@ extern crate rocket;
 // rocket的路由分两部分，handler函数注解一部分，一般就简化为“/”
 // 另外一部分在构建服务的时候mount挂载
 // 主要在mount里设置，所以handler函数注解一般简化为“/”
-// 但是也可以根据实际需求来分配一个确定的路由
+// 但是也可以根据实际需求来分配一个确定的路由，例如路径参数
 #[get("/")]
 async fn index() -> &'static str {
     "hello rocket!"
+}
+
+
+#[get("/<name>")]
+async fn hello(name: &str) -> String {
+    format!("Hello {}", name)
 }
 
 
@@ -17,9 +23,15 @@ async fn index() -> &'static str {
 // 异步使用rocket：：main注解main函数，rocketbuilder需要调用launch和await
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build()
+    let result = rocket::build()
+    .mount("/", routes![index])
     .mount("/hello", rocket::routes![index])
     .mount("/hi", routes![index])
+    .mount("/hello", routes![hello])
     .launch()
     .await;
+
+    if let Err(e) = result {
+        eprintln!("shutdown beacause: {:?}", e);
+    }
 }
