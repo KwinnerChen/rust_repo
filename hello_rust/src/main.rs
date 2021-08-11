7f3a883517ca6ddfa0e1f5369bc171535c590dda
KwinnerChen <chenkai.c.k@hotmail.com>
2021-08-11 14:26:39 +0800

    update:some tests

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_params() {
+        let a = 1;
+        fn chage_a(mut x: i32) -> i32 {
+            x += 1;
+            x
+        }
+
+        let b = chage_a(a);
+
+        println!("{}, {}", &a, &b);
+        assert_ne!(a, chage_a(a));
+    }
+}
